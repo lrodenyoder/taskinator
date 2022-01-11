496eed271212eafc3332604dc132895877427fdd
lrodenyoder <rodenlau000@gmail.com>
2022-01-10 18:44:39 -0600

    ability to add task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
-var buttonE1 = document.querySelector("#save-task");
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-buttonE1.addEventListener("click", function () {
-    alert("Button clicked");
-});
+var createTaskHandler = function () {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task";
+    tasksToDoEl.appendChild(listItemEl);
+};
+
+buttonEl.addEventListener("click", createTaskHandler);
+
+
